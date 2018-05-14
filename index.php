4538e20a65bbe93cd4253f9b5c3c482d4dbf1b19
KevinMunsch <ke.munsch@laposte.net>
2018-05-14 16:26:57 +0200

    fini

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+
+<html lang="fr" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+<!-- Exercice 1 -->
+
+<?php
+$nom="munsch";
+echo"$nom";
+
+// exercie 2
+
+$prenom="kevin";
+$age="22";
+
+echo" $prenom";
+echo" $age";
+
+// Exercice 3
+
+$km="125";
+
+echo" $km";
+
+//exercice 4
+
+$str="string";
+$entier=22;
+$decimal=22.5;
+$gagne=true;
+
+echo " $str"." $entier"." $decimal"." $gagne";
+
+// exercice 5
+
+$entier2=65;
+
+echo " $entier2";
+
+// exercice 6
+
+$nom2="dupont";
+
+echo " bonjour"." $nom2".",comment vas tu ?";
+
+// exercice 7
+
+echo " bonjour"." $nom"." $prenom".", tu as"." $age"." ans";
+
+// exercice 8
+
+$addition=3+4;
+$multi=5*20;
+$divi=45/5;
+
+echo " $addition"." $multi"." $divi";
+
+?>
+
+
+  </body>
+</html>
